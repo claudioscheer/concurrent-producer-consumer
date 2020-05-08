0d0a8de08e681195c9a0ab4cab6bf468ecc94e94
Claudio Scheer <claudioscheer@protonmail.com>
2020-05-07 21:37:40 -0300

    Lazy list tests

--- test/scripts/run-tests.sh
+++ test/scripts/run-tests.sh
@@ -4,6 +4,7 @@
 # ./scripts/run-tests.sh coarse-list "Coarse List"
 # ./scripts/run-tests.sh fine-list "Fine List"
 # ./scripts/run-tests.sh optimistic-list "Optimistic List"
+# ./scripts/run-tests.sh lazy-list "Lazy List"
 
 rm -rf ../bin
 cd ..
