d74ceeefddec9c7fcffd04ce74bf1b32c2a69a6d
Claudio Scheer <claudioscheer@protonmail.com>
2020-05-20 23:31:30 -0300

    LockFree list tests

--- test/scripts/run-tests.sh
+++ test/scripts/run-tests.sh
@@ -5,6 +5,7 @@
 # ./scripts/run-tests.sh fine-list "Fine List"
 # ./scripts/run-tests.sh optimistic-list "Optimistic List"
 # ./scripts/run-tests.sh lazy-list "Lazy List"
+# ./scripts/run-tests.sh lock-free-list "Lock Free List"
 
 rm -rf ../bin
 cd ..
